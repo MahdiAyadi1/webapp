f5605b5ded5fec25bbea2d8e54b79c92d58feed5
Mohamed Chaabouni <mohamed.chaa33@gmail.com>
2023-02-26 01:14:14 +0100

    v

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,11 +5,11 @@ const Home = ()=> {
     const [nav,setNav]= useState('map')
     return (
         <div className="home">
-        <Sidebar nav={nav}  setNav={setNav}/>
-        {nav=='map' && <LeafletMap />}
-        {nav=='map' && <Metroinfo/>}
-        {nav=='manageaccounts' &&<Manageaccounts />}
-        {nav=='timetable' && <Timetable />}
+            <Sidebar nav={nav}  setNav={setNav}/>
+            {nav=='map' && <LeafletMap />}
+            {nav=='map' && <Metroinfo/>}
+            {nav=='manageaccounts' &&<Manageaccounts />}
+            {nav=='timetable' && <Timetable />}
         </div>
     )
 }
