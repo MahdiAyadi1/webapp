9874803af97402f5e198cb6725faba9da690273e
Mahdi <mahdi.ayadi@ensi-uma.tn>
2023-03-29 23:06:39 +0100

    icon_on_leaflet

--- src/components/leafletmap/LeafletMap.js
+++ src/components/leafletmap/LeafletMap.js
@@ -8,41 +8,12 @@ import { lineone } from "./lineone";
 import {StationIcon} from './stationIcon'
 
 const LeafletMap = ()=> {
+
   var myIcon = L.icon({
     iconUrl: require("./metro.png"),
     iconSize: [36, 36],
     iconAnchor: [36, 36],
     popupAnchor: [-3, -76],
-});
-var station1Icon = L.icon({
-  iconUrl: require("./station1.png"),
-  iconSize: [36, 36],
-  iconAnchor: [36, 36],
-  popupAnchor: [-3, -76],
-});
-var station2Icon = L.icon({
-  iconUrl: require("./station2.png"),
-  iconSize: [36, 36],
-  iconAnchor: [36, 36],
-  popupAnchor: [-3, -76],
-});
-var station3Icon = L.icon({
-  iconUrl: require("./station3.png"),
-  iconSize: [36, 36],
-  iconAnchor: [36, 36],
-  popupAnchor: [-3, -76],
-});
-var station4Icon = L.icon({
-  iconUrl: require("./station4.png"),
-  iconSize: [36, 36],
-  iconAnchor: [36, 36],
-  popupAnchor: [-3, -76],
-});
-var station5Icon = L.icon({
-  iconUrl: require("./station5.png"),
-  iconSize: [36, 36],
-  iconAnchor: [36, 36],
-  popupAnchor: [-3, -76],
 });
   return (
     <div className="map">
@@ -65,54 +36,14 @@ var station5Icon = L.icon({
           <Tooltip>Metro 401</Tooltip>
         </Marker>
         {lineone.map((val) =>{ 
-          switch (val.ligne){
-            case("Ligne 1"):
             return (
-              // <Marker key={val.name} position={val.coordinates} icon={<StationIcon url="./station1.png"/>}>
-              <Marker key={val.name} position={val.coordinates} icon={station1Icon}>
-                <Popup>
-                  Station {val.name} <br/>{val.ligne}
-                </Popup>
-                <Tooltip>Station {val.name} <br/>{val.ligne}</Tooltip>
-              </Marker>
-            )
-            case("Ligne 2"):
-            return (
-              <Marker key={val.name} position={val.coordinates} icon={station2Icon}>
-                <Popup>
-                  Station {val.name} <br/>{val.ligne}
-                </Popup>
-                <Tooltip>Station {val.name} <br/>{val.ligne}</Tooltip>
-              </Marker>
-            )
-            case("Ligne 3"):
-            return (
-              <Marker key={val.name} position={val.coordinates} icon={station3Icon}>
-                <Popup>
-                  Station {val.name} <br/>{val.ligne}
-                </Popup>
-                <Tooltip>Station {val.name} <br/>{val.ligne}</Tooltip>
-              </Marker>
-            )
-            case("Ligne 4"):
-            return (
-              <Marker key={val.name} position={val.coordinates} icon={station4Icon}>
-                <Popup>
-                  Station {val.name} <br/>{val.ligne}
-                </Popup>
-                <Tooltip>Station {val.name} <br/>{val.ligne}</Tooltip>
-              </Marker>
-            )
-            case("Ligne 5"):
-            return (
-              <Marker key={val.name} position={val.coordinates} icon={station5Icon}>
-                <Popup>
-                  Station {val.name} <br/>{val.ligne}
-                </Popup>
-                <Tooltip>Station {val.name} <br/>{val.ligne}</Tooltip>
-              </Marker>
-            )
-          }
+               <Marker key={val.name} position={val.coordinates} icon={L.icon({
+                iconUrl: require(`./station${val.ligne.at(-1)}.png`),
+                iconSize: [36, 36],
+                iconAnchor: [36, 36],
+                popupAnchor: [-3, -76],
+              })}/>
+              )
         })
         }         
 
